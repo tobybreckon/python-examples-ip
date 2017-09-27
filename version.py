66c65e553b13ca543e30a105e625002d38000202
tobybreckon <toby.breckon@cranfield.ac.uk>
2017-09-27 13:18:51 +0100

    add versin test script

---
+++ version.py
@@ -0,0 +1,24 @@
+#####################################################################
+
+# Example : test if opencv environment is working
+
+# Author : Toby Breckon, toby.breckon@durham.ac.uk
+
+# Copyright (c) 2017 Toby Breckon
+#                    Durham University, UK
+# License : LGPL - http://www.gnu.org/licenses/lgpl.html
+
+#####################################################################
+
+import cv2
+import numpy as np
+import sys
+import math
+import matplotlib
+
+print("We are using OpenCV: " + cv2.__version__);
+print("We are using numpy: " + np.__version__);
+print("We are using matplotlib: " + matplotlib.__version__);
+print (".. and this is in Python: " + sys.version)
+
+#####################################################################
