17c7dc19a422f4858fd46cd7b2f5f74cec642ae8
Toby Breckon <toby@breckon.me.uk>
2019-10-23 07:29:33 +0100

    initial version of test script

---
+++ test_all.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+################################################################################
+
+# run a batch test over all the examples from the bash shell (linux)
+
+# Copyright (c) 2019 Dept Computer Science,
+#                    Durham University, UK
+# License : LGPL - http://www.gnu.org/licenses/lgpl.html
+
+################################################################################
+
+PYTHON_INTERPRETATOR=python3
+CAM_TO_TEST=0
+VIDEO_TO_TEST=video.avi
+
+echo
+echo Using $PYTHON_INTERPRETATOR with camera $CAM_TO_TEST and video $VIDEO_TO_TEST
+echo "Running test suite - press 'x' in OpenCV window to exist each example."
+echo
+
+################################################################################
+
+# run defaults
+
+for example in *.py
+do
+ echo "Testing example: " $example
+ $PYTHON_INTERPRETATOR $example
+ echo
+done
+
+################################################################################
+
+# run cam test
+
+for example in *.py
+do
+ echo "Testing example: " $example -c $CAM_TO_TEST
+ $PYTHON_INTERPRETATOR $example -c $CAM_TO_TEST
+ echo
+done
+
+################################################################################
+
+# run cam test and resize
+
+for example in *.py
+do
+ echo "Testing example: " $example -c $CAM_TO_TEST -r 0.25
+ $PYTHON_INTERPRETATOR $example -c $CAM_TO_TEST -r 0.25
+ echo
+done
+
+
+################################################################################
+
+# run video file test
+
+for example in *.py
+do
+ echo "Testing example: " $example $VIDEO_TO_TEST
+ $PYTHON_INTERPRETATOR $example $VIDEO_TO_TEST
+ echo
+done
+
+################################################################################
